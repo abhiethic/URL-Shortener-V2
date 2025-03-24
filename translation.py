c3e424664ac3bf588cdef3c618e7c5298b05e449
abhiethic <168088445+abhiethic@users.noreply.github.com>
2025-03-24 22:26:12 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -122,6 +122,19 @@ USER_ABOUT_MESSAGE = """
 {footer_text}
 •  🖼️ Banner image: {banner_image}
 """
+METHOD_MESSAGE = """
+Current Method: {shortener}
+    
+Methods Available:
+
+> `mdlink` - Change all the links of the post to your MDisk account first and then short to {shortener} link.
+
+> `shortener` - Short all the links of the post to {shortener} link directly.
+
+> `mdisk` - Save all the links of the post to your Mdisk account.
+    
+To change method, choose it from the following options:
+"""
 
 SHORTENER_API_MESSAGE = """<b>How To Connect API ?</b>
 
